0a47522b333f598091e4f78e2f1231b8b391a068
jeffryporter <jporter7@gmail.com>
2016-06-22 12:10:02 -0400

    Add query methods and join

--- src/main/java/com/theironyard/RestaurantTrackerController.java
+++ src/main/java/com/theironyard/RestaurantTrackerController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -20,20 +21,54 @@ public class RestaurantTrackerController
     @Autowired
     RestaurantRepository restaurants;
 
+    @PostConstruct
+    public void init()
+    {
+        if (users.count() == 0)
+        {
+            User user = new User("Jeff", "12345");
+            users.save(user);
+        }
+    }
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home(HttpSession session, Model model)
+    public String home(HttpSession session, Model model, Integer rating, String location, String search)
     {
         String username = (String) session.getAttribute("username");
+        Iterable<Restaurant> rests;
         if(username == null)
         {
             return "login";
         }
         else
         {
-            Iterable<Restaurant> rests = restaurants.findAll();
-            model.addAttribute("restaurants", rests);
-            return "home";
+            User user = users.findByName(username);
+
+            if (search != null)
+            {
+                rests = restaurants.searchLocation(search);
+            }
+            else
+            {
+                if (rating != null && location != null)
+                {
+                    rests = restaurants.findByRatingAndLocation(rating, location);
+                } else if (location != null)
+                {
+                    rests = restaurants.findByLocation(location);
+                } else if (rating != null)
+                {
+                    rests = restaurants.findByRatingGreaterThanEqual(rating);
+                } else
+                {
+                    rests = restaurants.findByUser(user);
+                }
+            }
+
         }
+        model.addAttribute("restaurants", rests);
+        return "home";
+
     }
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
@@ -61,9 +96,11 @@ public class RestaurantTrackerController
     }
 
     @RequestMapping(path = "/create-restaurant", method = RequestMethod.POST)
-    public String create(String name, String location, int rating, String comment)
+    public String create(HttpSession session,String name, String location, int rating, String comment)
     {
-        Restaurant r = new Restaurant(name, location, rating, comment);
+        String username = (String)session.getAttribute("username");
+        User user = users.findByName(username);
+        Restaurant r = new Restaurant(name, location, rating, comment, user);
         restaurants.save(r);
         return "redirect:/";
     }
